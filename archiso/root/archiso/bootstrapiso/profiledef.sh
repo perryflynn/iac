2bc52c29c06913d8058c86b7ff20178ae10e89a4
Christian <christian@serverless.industries>
2024-05-09 11:54:01 +0200

    include cloudinit into archiso

--- archiso/root/archiso/bootstrapiso/profiledef.sh
+++ archiso/root/archiso/bootstrapiso/profiledef.sh
@@ -12,7 +12,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
-pacman_conf="pacman.conf"
+#pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=(gzip -cn9)
