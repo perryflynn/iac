f42cca99408049e6a2c8f3e023c3e4932bd20e7f
Christian <christian@serverless.industries>
2024-11-13 15:53:10 +0100

    update package list and add cloud-init fuckup workaround

--- archiso/root/archiso/bootstrapiso/airootfs/root/customize_airootfs.sh
+++ archiso/root/archiso/bootstrapiso/airootfs/root/customize_airootfs.sh
@@ -2,11 +2,14 @@
 
 set -e -u -x
 
-# enable cloud-init services
-systemctl enable cloud-init-local
-systemctl enable cloud-init
-systemctl enable cloud-config
-systemctl enable cloud-final
+# https://gitlab.archlinux.org/archlinux/packaging/packages/cloud-init/-/issues/3#note_216601
+ln -s /bin/nc /bin/nc.openbsd
+sed -i '/Before=sysinit.target/d' /usr/lib/systemd/system/cloud-init-main.service
 
-# disable mirror reflector
-#systemctl disable reflector
+# enable cloud-init services
+systemctl enable cloud-init-local || true
+systemctl enable cloud-init-main.service
+systemctl enable cloud-init-network || true
+systemctl enable systemd-networkd || true
+systemctl enable systemd-timesyncd || true
+systemctl enable systemd-resolved || true
